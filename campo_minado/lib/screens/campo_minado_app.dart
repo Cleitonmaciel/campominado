83dc233131a4b38cec2db51f7878a951336985e0
Cleiton Maciel <cleitonmaciell@gmail.com>
2020-08-13 06:37:21 -0300

    conclusão do do jogo

---
+++ campo_minado/lib/screens/campo_minado_app.dart
@@ -0,0 +1,96 @@
+import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
+import 'package:campo_minado/screens/tabuleiro_widget.dart';
+import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
+import '../models/campo.dart';
+
+class CampoMinadoApp extends StatefulWidget {
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu;
+  Tabuleiro _tabuleiro;
+
+  void _reiniciar() {
+    setState(() {
+      _venceu = null;
+      _tabuleiro.reiniciar();
+    });
+  }
+
+  void _abrir(Campo campo) {
+    setState(() {
+      if (_venceu != null) {
+        return;
+      }
+
+      try {
+        campo.abrir();
+        if (_tabuleiro.resolvido) {
+          _venceu = true;
+        }
+      } on ExplosaoException {
+        _venceu = false;
+        _tabuleiro.revelarBombas();
+      }
+    });
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    setState(() {
+      if (_venceu != null) {
+        return;
+      }
+
+      campo.alternarMarcacao();
+      if (_tabuleiro.resolvido) {
+        _venceu = true;
+      }
+    });
+  }
+
+  Tabuleiro _getTabuleiro(double largura, double altura) {
+    if (_tabuleiro == null) {
+      int qtdeColunas = 15;
+      double tamanhoCampo = largura / qtdeColunas;
+      int qtdeLinhas = (altura / tamanhoCampo).floor();
+
+      _tabuleiro = Tabuleiro(
+        linhas: qtdeLinhas,
+        colunas: qtdeColunas,
+        qtdeBombas: 30,
+      );
+    }
+    return _tabuleiro;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: ResultadoWidget(
+          venceu: _venceu,
+          onReiniciar: _reiniciar,
+        ),
+        body: Container(
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return TabuleiroWidget(
+                tabuleiro: _getTabuleiro(
+                  constraints.maxWidth,
+                  constraints.maxHeight,
+                ),
+                onAbrir: _abrir,
+                onAlternarMarcacao: _alternarMarcacao,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
